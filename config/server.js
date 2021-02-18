e0de677ed91b7c023e9e3a7d6aae7f6b1b81f927
Fernando Britto <fernandobritto.web@gmail.com>
2021-02-18 07:41:58 -0300

    Initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'b193b6275c67ef96f9b289f231688ad4'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'c1214916-6cfa-42db-8ccb-1bdf59e0ac05'
+};
